9d97152e30ae8b7051ac8ba34634f8e40938f64a
Nikolay Kim <fafhrd91@gmail.com>
2017-09-17 22:12:31 -0700

    split ctx library

--- fectl/apps/__init__.py
+++
@@ -1,8 +0,0 @@
-from .aiohttp_config import AiohttpSettings
-
-AIOHTTP_SETTINGS = AiohttpSettings()
-
-
-APPS = {
-    "aiohttp": "fectl.apps.aiohttp.AiohttpRunner",
-}
--- fectl/errors.py
+++
@@ -1,8 +0,0 @@
-
-
-class ConfigurationError(Exception):
-    """ Exception raised on config error """
-
-
-class UnsupportedWorker(ConfigurationError):
-    """ Worker is not supported by application """
