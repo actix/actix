2ca84aa975f9e15561d17f665d75ce642f7b3602
Nikolay Kim <fafhrd91@gmail.com>
2017-10-11 18:45:11 -0700

    disable skeptic for windows

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 extern crate skeptic;
 
+#[cfg(unix)]
 fn main() {
     // generates doc tests for `README.md`.
     skeptic::generate_doc_tests(&["README.md"]);
+}
+
+#[cfg(not(unix))]
+fn main() {
 }
