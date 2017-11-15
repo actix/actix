478410b587437cb7b73b945aadb0efcdcd15baac
Callym <hi@callym.com>
2017-11-15 21:24:00 +0000

    add support for #[Message(result, error)], with better error handling

--- actix_derive/src/lib.rs
+++ actix_derive/src/lib.rs
@@ -30,7 +30,27 @@ macro_rules! create_derive(
 );
 
 create_derive!(actor, Actor, actor_derive);
-create_derive!(message, Message, message_derive, MessageResult, MessageError);
+create_derive!(message, Message, message_derive, MessageResult, MessageError, Message);
+
+fn exists_attribute(ast: &syn::DeriveInput, name: &str) -> bool {
+    ast.attrs.iter().find(|a| a.name() == name).is_some()
+}
+
+fn get_attribute_type_multiple(ast: &syn::DeriveInput, name: &str) -> Option<Vec<Option<syn::Ty>>> {
+    let attr = ast.attrs.iter().find(|a| a.name() == name);
+
+    if attr.is_none() {
+        return None;
+    }
+
+    let attr = attr.unwrap();
+
+    if let syn::MetaItem::List(_, ref vec) = attr.value {
+        Some(vec.iter().map(|m| meta_item_to_ty(m, name)).collect())
+    } else {
+        panic!("The correct syntax is #[{}(type, type, ...)]", name);
+    }
+}
 
 fn get_attribute_type(ast: &syn::DeriveInput, name: &str) -> Option<syn::Ty> {
     let attr = ast.attrs.iter().find(|a| a.name() == name);
@@ -43,19 +63,23 @@ fn get_attribute_type(ast: &syn::DeriveInput, name: &str) -> Option<syn::Ty> {
 
     if let syn::MetaItem::List(_, ref vec) = attr.value {
         if vec.len() != 1 {
-            panic!();
+            panic!("#[{}(type)] takes 1 parameter, given {}", name, vec.len());
         }
 
-        if let syn::NestedMetaItem::MetaItem(ref i) = vec[0] {
-            if let syn::MetaItem::Word(ref i) = *i {
-                let ty = syn::parse::ty(i.as_ref());
-                match ty {
-                    syn::parse::IResult::Done(_, ty) => return Some(ty),
-                    _ => panic!(""),
-                }
-            }
-        }
+        meta_item_to_ty(&vec[0], name)
+    } else {
+        panic!("The correct syntax is #[{}(type)]", name);
     }
+}
 
-    panic!();
+fn meta_item_to_ty(meta_item: &syn::NestedMetaItem, name: &str) -> Option<syn::Ty> {
+    if let syn::NestedMetaItem::MetaItem(syn::MetaItem::Word(ref i)) = *meta_item {
+        let ty = syn::parse::ty(i.as_ref());
+        match ty {
+            syn::parse::IResult::Done(_, ty) => Some(ty),
+            _ => None,
+        }
+    } else {
+        panic!("The correct syntax is #[{}(type)]", name);
+    }
 }
