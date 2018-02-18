d5079e5089ee6e78fe2892cc7602707db1c9ed5c
Nikolay Kim <fafhrd91@gmail.com>
2018-02-18 11:33:44 -0800

    add wip guide sections

--- build.rs
+++ build.rs
@@ -8,8 +8,18 @@ fn main() {
         // generates doc tests for `README.md`.
         skeptic::generate_doc_tests(
             &["README.md",
-              "guide/src/qs_1.md",
-              "guide/src/qs_2.md",
+              "guide/src/qs_01.md",
+              "guide/src/qs_02.md",
+              "guide/src/qs_03.md",
+              "guide/src/qs_04.md",
+              "guide/src/qs_05.md",
+              "guide/src/qs_06.md",
+              "guide/src/qs_07.md",
+              "guide/src/qs_08.md",
+              "guide/src/qs_09.md",
+              "guide/src/qs_10.md",
+              "guide/src/qs_11.md",
+              "guide/src/qs_12.md",
             ]);
     } else {
         let f = env::var("OUT_DIR").unwrap() + "/skeptic-tests.rs";
--- guide/src/SUMMARY.md
+++ guide/src/SUMMARY.md
@@ -1,4 +1,14 @@
 # Summary
 
-- [Quickstart](./qs_1.md)
-- [Getting Started](./qs_2.md)
+- [Quickstart](./qs_01.md)
+- [Getting Started](./qs_02.md)
+- [Actor](./qs_03.md)
+- [Address](./qs_04.md)
+- [Context](./qs_05.md)
+- [Arbiter](./qs_06.md)
+- [SyncArbiter](./qs_07.md)
+- [Stream](./qs_08.md)
+- [IO helpers](./qs_09.md)
+- [Supervisor](./qs_10.md)
+- [Registry](./qs_11.md)
+- [Helper actors](./qs_12.md)
