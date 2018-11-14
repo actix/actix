1a75b53afaf213ec14a7f0853e1ecfcc6d856865
Christopher Field <cfield2@gmail.com>
2018-11-14 18:00:30 -0500

    Add comments and documentation to all types

--- examples/echo.rs
+++ examples/echo.rs
@@ -56,20 +56,22 @@ use tokio::io;
 
 /// The message sent from the [`Stdin`] actor to a recipient.
 ///
-/// The Stdin actor publishes (sends) this message to a subscriber (recipient)
-/// and it is just a thin wrapper around a series of bytes from STDIN.
+/// The Stdin actor publishes (sends) this message to a subscriber (recipient).
+/// This message is just a thin wrapper around a series of bytes from STDIN.
 ///
 /// [`Stdin`]: struct.Stdin.html
 #[derive(Message)]
 struct Input(pub BytesMut);
 
+// A conversion helper implementation for converting BytesMut into the wrapper
+// Input message.
 impl From<BytesMut> for Input {
     fn from(b: BytesMut) -> Self {
         Input(b)
     }
 }
 
-/// The message received by the [`Stdout`] actor.
+/// The message handled by the [`Stdout`] actor.
 ///
 /// The Stdout actor writes the message, which is just a thin wrapper around a
 /// series of bytes, to STDOUT.
@@ -78,11 +80,9 @@ impl From<BytesMut> for Input {
 #[derive(Message)]
 struct Output(pub Bytes);
 
-// A simple conversion of an [`Input`] message to an [`Output`] message so that
-// the Stdin actor can easily "forward" its messages to the Stdout actor.
-//
-// [`Input`]: struct.Input.html
-// [`Output`]: struct.Output.html
+// A conversion helper of an Input message to an Output message so that
+// the Echo actor can easily convert the Input message from the Stdin actor to
+// the Output message of the Stdout actor.
 impl From<Input> for Output {
     fn from(i: Input) -> Self {
         Output(i.0.freeze())
@@ -103,6 +103,11 @@ impl From<Input> for Output {
 /// [`Output`]: struct.Output.html
 /// [`Stream`]: https://docs.rs/futures/0.1.25/futures/stream/trait.Stream.html
 struct Stdout {
+    /// The sender of the internal channel used to communicate with the STDOUT
+    /// thread.
+    ///
+    /// When this is dropped, the STDOUT thread will be stopped and the actix
+    /// system is shutdown.
     tx: UnboundedSender<Bytes>,
 }
 
@@ -145,8 +150,8 @@ impl Stdout {
     }
 }
 
-// Added trace statements to help demonstrate the interaction between the Stdin
-// and Stdout actors.
+// Added trace logging statements to help demonstrate the life-cycle of this
+// actor.
 impl Actor for Stdout {
     type Context = Context<Self>;
 
@@ -164,10 +169,10 @@ impl Actor for Stdout {
     }
 }
 
-// The Stdout actor receives Output messages.
+// The Stdout actor handles Output messages.
 //
-// The handler of the Output messages for the Stdout actor is relatively simple.
-// The Output message is sent to the STDOUT thread using the internal channel.
+// This is relatively simple. Output messages are sent to the STDOUT thread
+// using the internal channel as a series of bytes.
 impl Handler<Output> for Stdout {
     type Result = ();
 
@@ -177,7 +182,7 @@ impl Handler<Output> for Stdout {
     }
 }
 
-// A little conversion helper to clean up creation of a Stdout actor from an
+// A conversion helper to clean up creation of a Stdout actor from an
 // encoder/codec. Uses the current actix system.
 impl<E> From<E> for Stdout
     where E: Encoder<Item=Bytes, Error=Error> + Send + Clone + 'static
@@ -190,15 +195,21 @@ impl<E> From<E> for Stdout
 /// An actor that reads bytes from STDIN.
 ///
 /// This actor starts a separate thread to avoid blocking and uses a channel to
-/// receive bytes at an [`Input`] message from the separate thread. The channel
-/// only has one sender (tx). When STDIN receives an End-of-File (EOF) signal,
-/// the sender will be dropped. This causes the receiver within this actor to
-/// become resolved, i.e. completed. The resolution of the receiver stops this
-/// actor.
+/// receive a continuous stream of messages from the separate thread. The channel
+/// only has one sender (tx), which is moved to the separate thread. When STDIN
+/// receives an End-of-File (EOF) signal, the sender will be dropped. This
+/// causes the receiver within this actor to become resolved, i.e. completed.
+/// The resolution of the receiver stops this actor.
+///
+/// This actor is implemented as a publisher, in that it does not receive
+/// messages from other actors. Instead, it publishes [`Input`] messages to a
+/// subscriber (recipient).
 ///
 /// [`Input`]: struct.Input.html
 struct Stdin<D> {
+    /// The subscriber of the published Input messages.
     recipient: Recipient<Input>,
+    /// Decodes the stream of bytes from STDIN into framed messages.
     decoder: D,
 }
 
@@ -233,7 +244,7 @@ impl<D> Stdin<D>
 // A separate thread is started when this actor is started. The separate thread
 // reads bytes from STDIN and sends them to this actor using an internal
 // channel. This implementation of the Actor trait also includes trace
-// logging statements to demonstrate the interaction with the Stdout actor.
+// logging statements to demonstrate the life-cycle of this actor.
 impl<D> Actor for Stdin<D>
     where D: Decoder<Item=BytesMut, Error=Error> + Send + Clone + 'static
 {
@@ -271,6 +282,12 @@ impl<D> Actor for Stdin<D>
     }
 }
 
+// The Stdin actor handles the stream of frames from STDIN thread using a
+// channel. The receiver is "attached" to the execution context of this actor,
+// which means it must implement a stream handler. Using the helper conversion
+// for the Input message, i.e. `std::convert::From<BytesMut>`, the frames of
+// bytes are easily transformed into Input messages and published/sent to the
+// subscriber/recipient.
 impl<D> StreamHandler<BytesMut, ()> for Stdin<D>
     where D: Decoder<Item=BytesMut, Error=Error> + Send + Clone + 'static
 {
@@ -280,11 +297,30 @@ impl<D> StreamHandler<BytesMut, ()> for Stdin<D>
     }
 }
 
+/// An actor that implements the main functionality of the example/application.
+///
+/// This actor subscribes to Input messages from the [`Stdin`] actor, converts
+/// the [`Input`] message into an [`Output`] message, and sends the Output
+/// message to the [`Stdout`] actor. In this fashion, the actor echos, forwards,
+/// and/or copies the contents from STDIN to STDOUT.
+///
+/// [`Input`]: struct.Input.html
+/// [`Output`]: struct.Output.html
+/// [`Stdin`]: struct.Stdin.html
+/// [`Stdout`]: struct.Stdout.html
 struct Echo {
+    /// The address of the [`Stdout`] actor.
+    ///
+    /// [`Stdout`]: struct.Stdout.html
     stdout: Addr<Stdout>,
 }
 
 impl Echo {
+    /// Creates a new `Echo` actor.
+    ///
+    /// Only the address of the [`Stdout`] actor is needed.
+    ///
+    /// [`Stdout`]: struct.Stdout.html
     pub fn new(stdout: Addr<Stdout>) -> Self {
         Echo {
             stdout: stdout,
@@ -292,10 +328,29 @@ impl Echo {
     }
 }
 
+// Added trace logging statements to demonstrate the life-cycle of this actor.
 impl Actor for Echo {
     type Context = Context<Self>;
+
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        trace!("STDOUT started");
+    }
+
+    fn stopping(&mut self, _ctx: &mut Self::Context) -> Running {
+        trace!("STDOUT stopping");
+        Running::Stop
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        trace!("STDOUT stopped");
+    }
 }
 
+// Since the Echo actor is a recipient, or subscriber, of messages from the
+// Stdin actor, it must implement a handler for Input messages. The
+// implementation is very simple using the conversion helpers and the address of
+// the Stdout actor. The Input message is converted to an Output message and
+// sent to the Stdout actor.
 impl Handler<Input> for Echo {
     type Result = ();
 
@@ -305,14 +360,35 @@ impl Handler<Input> for Echo {
     }
 }
 
+// A conversion helper to create an Echo actor from a Stdout actor.
 impl From<Addr<Stdout>> for Echo {
     fn from(s: Addr<Stdout>) -> Self {
         Echo::new(s)
     }
 }
 
+/// A wrapper codec for the [`tokio::codec::LinesCodec`].
+///
+/// Since the `tokio::codec::LinesCodec` decodes and encodes items of the
+/// [`String`] type, it cannot be directly used with the [`Stdin`] and
+/// [`Stdout`] actors. The `Stdin` and `Stdout` actors use decoders and
+/// encoders with [`BytesMut`] and [`Bytes`] items, respectively. Thus, a new
+/// codec type that wraps the `tokio::codec::LinesCodec` and handles the
+/// conversion of `String` items to `BytesMut` and `Bytes` items is implemented.
+/// By wrapping the `tokio::codec::LinesCodec`, the functionality is
+/// maintained and the type requirements for using the `Stdin` and `Stdout`
+/// actors are met.
+///
+/// [`Bytes`]: https://carllerche.github.io/bytes/bytes/struct.Bytes.html
+/// [`BytesMut`]: https://carllerche.github.io/bytes/bytes/struct.BytesMut.html
+/// [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
+/// [`Stdin`]: struct.Stdin.html
+/// [`Stdout`]: struct.Stdout.html
+/// [`tokio::codec::LinesCodec`]: https://tokio-rs.github.io/tokio/tokio/codec/struct.LinesCodec.html
 #[derive(Clone)]
 struct LinesCodec {
+    /// The tokio codec that actually implements reading and writing strings
+    /// terminated by an End-of-Line (EOL) marker.
     inner: tokio::codec::LinesCodec,
 }
 
@@ -343,12 +419,36 @@ impl Default for LinesCodec {
 }
 
 fn main() {
+    // Initialize the logger.
+    //
+    // Use `RUST_LOG=echo=trace cargo run --example echo` to display only the
+    // logging statements from this example. Running `RUST_LOG=trace cargo run
+    // --example echo` will result in logging statements from the entire tokio
+    // and mio stack to be displayed.
     env_logger::init();
+
+    // Start the actix system. Note, the Stdin and Stdout actors will
+    // automatically create the separate threads to read and writing from STDIN
+    // and STDOUT in a non-blocking fashion, respectively. All that is required
+    // is to create and start the three actors, Stdin, Stdout, and Echo, in the
+    // proper order.
+    //
+    // Notice because of all of the conversion helpers that are implemented for
+    // each actor, running the echo example is essentially on line of code.
     let code = System::run(|| {
         Stdin::new(
             LinesCodec::default(),
+            // Because the Stdin and Stdout actors are implemented with generic
+            // decoders and encoders, respectively, it is relatively easy to modify
+            // this example to read lines of text from STDIN but have STDOUT write
+            // the lines in an entirely different format. Simply change the
+            // encoder/codec for the Stdout actor.
             Echo::new(Stdout::from(LinesCodec::default()).start()).start().recipient()
         ).start();
     });
+    // Exit the example/application with an appropriate exit code. When the
+    // example/application successfully terminates, the error code will be zero
+    // (0), which is convention. On failure, a non-zero value is used for the
+    // exit code.
     std::process::exit(code);
 }
