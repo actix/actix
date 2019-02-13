c7aaa24ded0b034bcfbd6a9f5ad82e68ba4360f7
Ewan Higgs <ewan_higgs@yahoo.co.uk>
2019-02-13 22:22:58 +0100

    Add ring test. Also cargo run --release --example ping doesn't work with LTO.

---
+++ examples/ring.rs
@@ -0,0 +1,90 @@
+extern crate actix;
+extern crate futures;
+
+use actix::*;
+
+use std::time::SystemTime;
+use std::env;
+
+/// A payload with a counter
+struct Payload(usize);
+
+impl Message for Payload {
+    type Result = ();
+}
+
+struct Node { 
+    limit: usize,
+    next: Recipient<Payload>,
+}
+
+impl Actor for Node {
+    type Context = Context<Self>;
+}
+
+impl Handler<Payload> for Node {
+    type Result = ();
+
+    fn handle(&mut self, msg: Payload, _: &mut Context<Self>) {
+        if msg.0 >= self.limit {
+            println!("Reached limit of {} (payload was {})", self.limit, msg.0);
+            System::current().stop();
+            return;
+        }
+        self.next.do_send(Payload(msg.0 + 1)).expect("Unable to send payload");
+    }
+}
+
+fn print_usage_and_exit() {
+    eprintln!("Usage; actix-test <num-nodes> <num-times-message-around-ring>");
+    ::std::process::exit(1);
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<_>>();
+    if  args.len() < 3 {
+        print_usage_and_exit();
+    }
+    let mut num_nodes = 2; 
+    if let Ok(arg_num_nodes) = args[1].parse::<usize>() {
+        if arg_num_nodes <= 1 {
+            eprintln!("Number of nodes must be > 1");
+            ::std::process::exit(1);
+        }
+        num_nodes = arg_num_nodes;
+    } else {
+        print_usage_and_exit();
+    }
+    let num_nodes = num_nodes;
+
+    let mut ntimes = 1;
+    if let Ok(arg_ntimes) = args[2].parse::<usize>() {
+        ntimes = arg_ntimes;
+    } else {
+        print_usage_and_exit();
+    }
+    let ntimes = ntimes; // make constant;
+
+    let system = System::new("test");
+
+
+    println!("Setting up nodes");
+    let _: Addr<_> = Node::create(move|ctx| {
+        let first_addr : Addr<_> = ctx.address();
+        let mut prev_addr: Addr<_> = Node{limit: num_nodes * ntimes, next: first_addr.recipient()}.start();
+        prev_addr.do_send(Payload(0));
+
+        for _ in 2..num_nodes {
+            prev_addr = Node{limit: num_nodes * ntimes, next: prev_addr.recipient()}.start();
+        }
+
+        Node{limit: num_nodes * ntimes, next: prev_addr.recipient()}
+    });
+
+    let now = SystemTime::now();
+    system.run();
+    match now.elapsed() {
+        Ok(elapsed) => println!("Time taken: {}.{:06}", elapsed.as_secs(), elapsed.subsec_micros()),
+        Err(e) => println!("An error occured: {:?}", e)
+    }
+}
