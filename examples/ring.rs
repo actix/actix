527e39832c93bbbece37035f822c6de9c28c6dd1
Ewan Higgs <ewan_higgs@yahoo.co.uk>
2019-02-14 13:18:20 +0100

    Fixes based on @Douman's feedback.
    Using ! is a nice way to let the compiler know that the else branch
    doesn't need to make sense. :)

--- examples/ring.rs
+++ examples/ring.rs
@@ -43,7 +43,7 @@ impl Handler<Payload> for Node {
     }
 }
 
-fn print_usage_and_exit() {
+fn print_usage_and_exit() -> ! {
     eprintln!("Usage; actix-test <num-nodes> <num-times-message-around-ring>");
     ::std::process::exit(1);
 }
@@ -53,25 +53,21 @@ fn main() {
     if  args.len() < 3 {
         print_usage_and_exit();
     }
-    let mut num_nodes = 2;
-    if let Ok(arg_num_nodes) = args[1].parse::<usize>() {
+    let n_nodes = if let Ok(arg_num_nodes) = args[1].parse::<usize>() {
         if arg_num_nodes <= 1 {
             eprintln!("Number of nodes must be > 1");
             ::std::process::exit(1);
         }
-        num_nodes = arg_num_nodes;
+        arg_num_nodes
     } else {
         print_usage_and_exit();
-    }
-    let num_nodes = num_nodes;
+    };
 
-    let mut ntimes = 1;
-    if let Ok(arg_ntimes) = args[2].parse::<usize>() {
-        ntimes = arg_ntimes;
+    let n_times = if let Ok(arg_ntimes) = args[2].parse::<usize>() {
+        arg_ntimes
     } else {
-        print_usage_and_exit();
-    }
-    let ntimes = ntimes; // make constant;
+        print_usage_and_exit()
+    };
 
     let system = System::new("test");
 
@@ -79,20 +75,20 @@ fn main() {
     println!("Setting up nodes");
     let _: Addr<_> = Node::create(move|ctx| {
         let first_addr : Addr<_> = ctx.address();
-        let mut prev_addr: Addr<_> = Node{limit: num_nodes * ntimes, next: first_addr.recipient()}.start();
+        let mut prev_addr: Addr<_> = Node{limit: n_nodes * n_times, next: first_addr.recipient()}.start();
         prev_addr.do_send(Payload(0));
 
-        for _ in 2..num_nodes {
-            prev_addr = Node{limit: num_nodes * ntimes, next: prev_addr.recipient()}.start();
+        for _ in 2..n_nodes {
+            prev_addr = Node{limit: n_nodes * n_times, next: prev_addr.recipient()}.start();
         }
 
-        Node{limit: num_nodes * ntimes, next: prev_addr.recipient()}
+        Node{limit: n_nodes * n_times, next: prev_addr.recipient()}
     });
 
     let now = SystemTime::now();
     system.run();
     match now.elapsed() {
-        Ok(elapsed) => println!("Time taken: {}.{:06}", elapsed.as_secs(), elapsed.subsec_micros()),
+        Ok(elapsed) => println!("Time taken: {}.{:06} seconds", elapsed.as_secs(), elapsed.subsec_micros()),
         Err(e) => println!("An error occured: {:?}", e)
     }
 }
