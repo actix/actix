a95e7a7986b8f110fb104dd8607200701f02f0cc
pandaman <kointosudesuyo@infoseek.jp>
2019-09-05 00:02:39 +0900

    Add Context::connected (#266)
    * Add `Context::connected`
    
    * update CHANGES.md and added comment

---
+++ tests/test_connected.rs
@@ -0,0 +1,36 @@
+use actix::prelude::*;
+use std::time::Duration;
+use tokio_timer::sleep;
+
+struct MyActor;
+
+impl Actor for MyActor {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        ctx.run_interval(Duration::from_millis(100), |_this, ctx| {
+            if !ctx.connected() {
+                ctx.stop();
+            }
+        });
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        System::current().stop()
+    }
+}
+
+#[test]
+fn test_connected() {
+    System::run(move || {
+        Arbiter::spawn_fn(move || {
+            let addr = MyActor::start(MyActor);
+            sleep(Duration::from_millis(350))
+                .map(move |()| {
+                    drop(addr);
+                })
+                .map_err(|_| ())
+        });
+    })
+    .unwrap();
+}
