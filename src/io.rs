da19f87353f0d72bb2a74753e343f7a0ffdd7fe1
Aaron Hill <aa1ronham@gmail.com>
2020-02-19 20:54:00 -0500

    Remove uses of `Pin::new_unchecked` in `WriterFut`
    The struct `WriterFut` is private, and is only ever constructed when
    `T: Unpin` is known to hold. This lets us add a `T: Unpin` bound to the
    definition of `WriterFut`, and use `Pin::new` instead of
    `Pin::new_unchecked`.

--- src/io.rs
+++ src/io.rs
@@ -142,7 +142,7 @@ impl<T: AsyncWrite, E: From<io::Error> + 'static> Writer<T, E> {
 
 struct WriterFut<T, E, A>
 where
-    T: AsyncWrite,
+    T: AsyncWrite + Unpin,
     E: From<io::Error>,
 {
     act: PhantomData<A>,
@@ -177,7 +177,7 @@ where
         let mut io = this.inner.1.borrow_mut();
         inner.task = None;
         while !inner.buffer.is_empty() {
-            match unsafe { Pin::new_unchecked(io.deref_mut()) }
+            match Pin::new(io.deref_mut())
                 .poll_write(task, &inner.buffer)
             {
                 Poll::Ready(Ok(n)) => {
@@ -216,7 +216,7 @@ where
         }
 
         // Try flushing the underlying IO
-        match unsafe { Pin::new_unchecked(io.deref_mut()) }.poll_flush(task) {
+        match Pin::new(io.deref_mut()).poll_flush(task) {
             Poll::Ready(Ok(_)) => (),
             Poll::Pending => return Poll::Pending,
             Poll::Ready(Err(ref e)) if e.kind() == io::ErrorKind::WouldBlock => {
