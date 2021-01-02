84732425b455af834918eeb03cdd7a51f3c96706
Rob Ede <robjtede@icloud.com>
2021-01-02 15:17:14 +0000

    migrate Chris-Ricketts/actix-broker (#455)
    * migrate Chris-Ricketts/actix-broker
    
    * add actix team to license
    
    * update readme
    
    * address clippy issues
    
    * no default features on broker examples

---
+++ actix-broker/README.md
@@ -0,0 +1,12 @@
+# actix-broker
+
+> A message broker for the Actix actor framework.
+
+[![crates.io](https://img.shields.io/crates/v/actix-broker?label=latest)](https://crates.io/crates/actix-broker)
+[![Documentation](https://docs.rs/actix-broker/badge.svg?version=0.3.1)](https://docs.rs/actix-broker/0.3.1)
+
+
+Subscribe to and issue Messages easily in an asynchronous or synchronous manner.
+
+
+Original code by @chris-ricketts. [Original repository.](https://github.com/chris-ricketts/actix-broker)
---
+++ actix-broker/tests/general.rs
@@ -0,0 +1,61 @@
+extern crate actix;
+extern crate actix_broker;
+
+use actix::prelude::*;
+use actix_broker::{BrokerIssue, BrokerSubscribe, SystemBroker};
+
+use std::time::Duration;
+
+#[derive(Clone, Message)]
+#[rtype(result = "()")]
+struct TestMessageOne(u8);
+
+struct TestActorOne;
+
+struct TestActorTwo;
+
+impl Actor for TestActorOne {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.subscribe_sync::<SystemBroker, TestMessageOne>(ctx);
+        ctx.run_later(Duration::from_millis(250), |a, _| {
+            a.issue_async::<SystemBroker, _>(TestMessageOne(255))
+        });
+    }
+}
+
+impl Actor for TestActorTwo {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.subscribe_sync::<SystemBroker, TestMessageOne>(ctx);
+    }
+}
+
+impl Handler<TestMessageOne> for TestActorOne {
+    type Result = ();
+
+    fn handle(&mut self, msg: TestMessageOne, _ctx: &mut Self::Context) {
+        assert_eq!(msg.0, 125);
+        System::current().stop();
+    }
+}
+
+impl Handler<TestMessageOne> for TestActorTwo {
+    type Result = ();
+
+    fn handle(&mut self, msg: TestMessageOne, _ctx: &mut Self::Context) {
+        assert_eq!(msg.0, 255);
+        self.issue_async::<SystemBroker, _>(TestMessageOne(125));
+    }
+}
+
+#[test]
+fn it_all_works() {
+    System::run(|| {
+        TestActorOne.start();
+        TestActorTwo.start();
+    })
+    .unwrap();
+}
