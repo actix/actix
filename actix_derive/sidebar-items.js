ae02ebfeaabae2f30fbee2c0e7eaef76c52e8afb
robjtede <robjtede@icloud.com>
2021-02-04 23:22:01 +0000

    Deploying to gh-pages from  @ 2ad96a8173801f1d98780df056e56fcfa0f9826b 🚀

--- actix_derive/sidebar-items.js
+++ actix_derive/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"derive":[["Message",""],["MessageResponse",""]]});
+initSidebarItems({"attr":[["main","Marks async function to be executed by Actix system."],["test","Marks async test function to be executed by Actix system."]],"derive":[["Message",""],["MessageResponse",""]]});
