df8d382d71a1d5de547bfd12b20567c3fd0ff916
geo-ant <54497890+geo-ant@users.noreply.github.com>
2022-02-26 13:52:55 +0100

    implement partial eq for weak recipients, TODO: add test

--- actix/src/address/channel.rs
+++ actix/src/address/channel.rs
@@ -92,6 +92,8 @@ where
     /// Returns [`None`] if the actor has since been dropped.
     fn upgrade(&self) -> Option<Box<dyn Sender<M> + Sync>>;
     fn boxed(&self) -> Box<dyn WeakSender<M> + Sync>;
+
+    fn hash(&self) -> usize;
 }
 
 /// The transmission end of a channel which is used to send values.
@@ -602,6 +604,11 @@ where
 
     fn boxed(&self) -> Box<dyn WeakSender<M> + Sync> {
         Box::new(self.clone())
+    }
+
+    fn hash(&self) -> usize {
+        let hash: *const _ = self.inner.as_ptr();
+        hash as usize
     }
 }
 
