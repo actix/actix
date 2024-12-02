63299e0047a45adb8fe3fbe405009e8835c5aee5
Rob Ede <robjtede@icloud.com>
2024-12-02 10:08:14 +0200

    chore: address clippy

--- actix/src/sync.rs
+++ actix/src/sync.rs
@@ -24,13 +24,14 @@ use crate::{
 };
 
 /// [`SyncArbiter`] provides the resources for a single Sync Actor to run on a dedicated
-/// thread or threads. This is generally used for CPU bound concurrent workloads. It's
-/// important to note, that the [`SyncArbiter`] generates a single address for the pool
-/// of hosted Sync Actors. Any message sent to this Address, will be operated on by
-/// a single Sync Actor from the pool.
+/// thread or threads.
 ///
-/// Sync Actors have a different lifecycle compared to Actors on the System
-/// Arbiter. For more, see `SyncContext`.
+/// This is generally used for CPU bound concurrent workloads. It's important to note, that the
+/// [`SyncArbiter`] generates a single address for the pool of hosted Sync Actors. Any message sent
+/// to this Address, will be operated on by a single Sync Actor from the pool.
+///
+/// Sync Actors have a different lifecycle compared to Actors on the System Arbiter. For more, see
+/// [`SyncContext`].
 ///
 /// # Examples
 ///
